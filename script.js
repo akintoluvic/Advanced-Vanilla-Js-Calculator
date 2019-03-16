086308db1c8d7eb6da80ef7aca0256f5dbdbbc65
akintoluvic@gmail.com <akintoluvic@gmail.com>
2019-03-16 20:24:25 +0100

    Update script.js
    corrected event listener

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const calculator = document.querySelector('.calculator');
-const keys = document.querySelector('.calculator__keys');
+const keys = calculator.querySelector('.calculator__keys');
 
-keys.addEventListener('click', e => {
-    if (e.target.matches('button')) {
-       console.log('keys'); 
+keys.addEventListener("click", e => {
+    if (e.target.matches("button")) {
+       console.log('keys....'); 
     }
     
-})
+});
