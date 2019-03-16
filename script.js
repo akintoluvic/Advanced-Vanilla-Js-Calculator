aefa98ffc99619dabeeb52adbfc1f734f5199f36
akintoluvic@gmail.com <akintoluvic@gmail.com>
2019-03-16 23:03:53 +0100

    Update script.js
    All key types now log to console

--- script.js
+++ script.js
@@ -6,7 +6,18 @@ keys.addEventListener('click', e => {
         const key = e.target;
         const action = key.dataset.action;
         if (!action) {
-            console.log('no key');
+            console.log('number key');
+        } else if (action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide') {
+            console.log('operator key');
+        } else if (action === 'decimal') {
+            console.log('decimal key');
+        } else if (action === 'clear') {
+            console.log('clear key');
+        }  else if (action === 'calculate') {
+            console.log('equal key');
         }
     }
     
