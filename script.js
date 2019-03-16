3c5c6bb1cd8db4457cd84441c3ee172341056300
akintoluvic@gmail.com <akintoluvic@gmail.com>
2019-03-16 23:02:44 +0100

    Fix event handling bug
    removed script file from the top and added it to the base of the index.html page

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const calculator = document.querySelector('.calculator');
-const keys = calculator.querySelector('.calculator__keys');
+const keys = document.querySelector('.calculator__keys');
 
-keys.addEventListener("click", e => {
+keys.addEventListener('click', e => {
     if (e.target.matches("button")) {
-       console.log('keys....'); 
+        const key = e.target;
+        const action = key.dataset.action;
+        if (!action) {
+            console.log('no key');
+        }
     }
     
 });
